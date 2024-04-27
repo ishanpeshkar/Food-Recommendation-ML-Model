d3b12df08850e55fd4a3efab1a5950c843cee4d1
Ishan Peshkar <114394219+ISHANPESHKAR@users.noreply.github.com>
2024-04-27 20:57:32 +0530

    Update App2.py

--- App2.py
+++ App2.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('file.html')
+    return render_template('index.html')
 
 """ @app.route('/predict', methods=['POST'])
 def predict_FoodName():
